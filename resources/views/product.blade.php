b81536e2ce37e99dd44723316771f1970b44ae25
Adam Kopec <a.kopec.st@spseiostrava.cz>
2021-06-03 22:39:34 +0200

    make it better

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,7 +4,7 @@
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6 bg-light">
     <div class="flex flex-col md:flex-row -mx-4">
       <div class="md:flex-1 px-4">
-          <img src="public/storage/photo/product/{{$product->main_photo}}" alt="" width="250px" height="250px" class="float-left">
+          <img src="/storage/photo/product/{{$product->main_photo}}" alt="" width="250px" height="250px" class="float-left">
           <h1 class="text-primary">{{$product->name}}</h1>
          <h5>Výrobce: <a href="https://www.{{$producer->url}}" style="text-decoration: none">{{$producer->name}}</a></h5>
         <h5>Hodnocení: nestíhám :)</h5>
